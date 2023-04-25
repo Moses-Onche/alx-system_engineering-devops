f64cfdd3a824796c87a3c3712624d141fbb05217
Moses-Onche <onchemoses38@gmail.com>
2023-04-25 01:07:39 +0100

    A script that prints a todo list item from a REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-#  A script that uses a REST API for a given employee ID
-#+ and returns a TODO list progress.
+# A script that uses a REST API for a given employee ID
+# and returns a TODO list progress.
 import sys
 import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo_items = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    items = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = []
-    for i in todo_items:
+    for i in items:
         if i.get("completed") is True:
             completed.append(i.get("title"))
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee.get("name"), len(completed), len(todo_items)))
+        employee.get("name"), len(completed), len(items)))
     for item in completed:
         print("\t {}".format(item))
