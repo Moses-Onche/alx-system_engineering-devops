71ad3be7a467c81175814d3828c3eb8a86e96598
Moses-Onche <onchemoses38@gmail.com>
2023-04-25 12:44:34 +0100

    A script that exports todo list with employeer records to a JSON file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that exports to-do list to JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as j_file:
+        json.dump({
+            person.get("id"): [{
+                "task": item.get("title"),
+                "completed": item.get("completed"),
+                "username": person.get("username")
+            } for item in requests.get(url + "todos",
+                                    params={"userId": person.get("id")}).json()]
+            for person in employee}, j_file)
