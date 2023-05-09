5d7ec57688483f9e2342cb51e65646cd967fe0db
Moses-Onche <onchemoses38@gmail.com>
2023-05-09 23:32:57 +0100

    Function that queries Reddit API to get num of subs under a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Describes a function that gets num of subscribers in a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function to query API and return number of subscribers."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "linux-Moses-Onche"}
+
+    response = requests.get(url, allow_redirects=False, headers=header)
+    if resp.status_code != 200:
+        return 0
+    else:
+        data = resp.json()
+        return data['data']['subscribers']
+
